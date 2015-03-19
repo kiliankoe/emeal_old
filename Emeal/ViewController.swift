dd6829b56819a5d28435d5d1bf0dad672699295b
Kilian Koeltzsch <me@kilian.io>
2015-03-19 11:05:46 +0100

    Initial Commit

---
+++ Emeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emeal
+//
+//  Created by Kilian Költzsch on 19/03/15.
+//  Copyright (c) 2015 kilian.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
