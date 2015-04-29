01d50322e79afc54931b9b9ff56db08d71dea63d
Kilian Koeltzsch <me@kilian.io>
2015-04-29 16:03:38 +0200

    Rename TransactionTableViewC... to TransactionsTVC

--- Emeal/TransactionsTVC.swift
+++ Emeal/TransactionsTVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TransactionsTableViewController: UITableViewController {
+class TransactionsTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
