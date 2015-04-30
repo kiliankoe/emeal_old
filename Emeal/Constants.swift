17072609fd5ce86c2015d1c72a915836224bc4a7
Kilian Koeltzsch <me@kilian.io>
2015-04-30 18:36:53 +0200

    Add Constants.swift with menu URL

---
+++ Emeal/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Emeal
+//
+//  Created by Kilian Költzsch on 29/04/15.
+//  Copyright (c) 2015 kilian.io. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+	static let mensaURL = "http://www.studentenwerk-dresden.de/mensen/speiseplan/"
+}
