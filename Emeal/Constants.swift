1311744ab2e98d42824b5a51acf1dbf4fc1523fe
Kilian Koeltzsch <me@kilian.io>
2015-08-26 00:47:09 +0200

    update URLs
    I might have something planned^^

--- Emeal/Constants.swift
+++ Emeal/Constants.swift
@@ -9,12 +9,20 @@
 import Foundation
 
 struct Constants {
-	static let spFeedURL = NSURL(string: "https://www.studentenwerk-dresden.de/feeds/speiseplan.rss")!
-	static let spFeedURLTomorrow = NSURL(string: "?tag=morgen", relativeToURL: spFeedURL)!
+	// Speiseplan
+	static let spMainURL = NSURL(string: "https://www.studentenwerk-dresden.de/mensen/speiseplan/")!
+	static let spMainTomorrowURL = NSURL(string: "morgen.html", relativeToURL: spMainURL)!
 	static func spDetailURL(id: Int) -> NSURL {
 		return NSURL(string: "https://www.studentenwerk-dresden.de/mensen/speiseplan/details-\(id).html")!
 	}
 
+	static let spMainURLEgg = NSURL(string: "http://www.studentenwerk-dresden.de.saxophone.parallelnetz.de/mensen/speiseplan/")!
+	static let spMainTomorrowURLEgg = NSURL(string: "morgen.html", relativeToURL: spMainURLEgg)!
+	static func spDetailURLEgg(id: Int) -> NSURL {
+		return NSURL(string: "http://www.studentenwerk-dresden.de.saxophone.parallelnetz.de/mensen/speiseplan/details-\(id).html")!
+	}
+
+	// Kartenservice
 	static let ksBaseURL = NSURL(string: "https://kartenservice.studentenwerk-dresden.de/KartenService/")!
 	static let ksTransactionsURL = NSURL(string: "Transaktionen.php", relativeToURL: ksBaseURL)!
 	static let ksUserDataURL = NSURL(string: "KartenDaten.php", relativeToURL: ksBaseURL)!
