b63b47720be19f7a2795a862c72dba6b5414cd3b
Kilian Koeltzsch <me@kilian.io>
2017-06-07 16:17:58 +0200

    add corenfc demo code

--- Emeal/ViewController.swift
+++ Emeal/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let _ = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(error)
+    }
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        print(messages)
+    }
+}
